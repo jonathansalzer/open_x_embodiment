fix scaling issues in finetuning

colabs/Minimal_Training_Example.py
@@ -923,7 +923,7 @@ def umi_map_action(to_step: rlds.Step, from_step: rlds.Step):
         output_high=1.75,
     )
   
-  to_step['action']['world_vector'] = from_step['action']['world_vector']
+  to_step['action']['world_vector'] = [pos_x, pos_y, pos_z]
   
   rot_x = rescale_value(
         from_step['action']['rotation_delta'][0],
@@ -2926,9 +2926,9 @@ class TrainState:
   opt_state: optax.OptState
   batch_stats: Any
 
-# base_checkpoint_path = "/home/jonathan/Thesis/ROS2_RT-1-X/ros2_ws/src/ros2_rt_1_x/ros2_rt_1_x/checkpoints/rt_1_x_jax"
+base_checkpoint_path = "/home/jonathan/Thesis/ROS2_RT-1-X/ros2_ws/src/ros2_rt_1_x/ros2_rt_1_x/checkpoints/rt_1_x_jax"
 # base_checkpoint_path = "/home/jonathan/Thesis/open_x_embodiment/training_results/train_1721567438_lr_0.0001_eps_1e-07/checkpoint_2000"
-base_checkpoint_path = "/home/jonathan/Thesis/open_x_embodiment/training_results/auto_save_and_reload"
+# base_checkpoint_path = "/home/jonathan/Thesis/open_x_embodiment/training_results/auto_save_and_reload"
 
 checkpoint_state_dict = checkpoints.restore_checkpoint(base_checkpoint_path, None)
 
@@ -3047,7 +3047,7 @@ def rt1_loss(
   # `action` is dict of (B, T, ...), we combine actions into B*T batch to
   # tokenize.
   action = jax.tree_map(lambda x: jnp.reshape(x, (bs * seqlen, -1)), action)
-  labels = tokenize_action(action, vocab_size=vocab_size)
+  labels = tokenize_action(action, vocab_size=vocab_size, world_vector_range=model.world_vector_range)
   labels = jax.tree_map(lambda x: jnp.reshape(x, (bs, seqlen, -1)), labels)
   labels = labels[:, :, :, None]  # labels should be (B, seqlen, 11, 1)
 
@@ -3082,7 +3082,7 @@ print(checkpoint_state_dict["opt_state"])
 
 # @title Set up the functions for training
 
-UMI_LEARNING_RATE = 1e-6
+UMI_LEARNING_RATE = 1e-4
 UMI_EPSILON = 1e-7
 
 # LEARNING RATE FROM OCTO